526f849a3286bd7298b38e1dd687a45599fc88fd
CamilaM16 <camila.malaga.60114@usalesiana.edu.bo>
2023-10-09 09:05:18 -0400

    Configure cores and add unsaved files

--- TodoListApi/Program.cs
+++ TodoListApi/Program.cs
@@ -1,8 +1,20 @@
 using TodoListApi.Models;
 using TodoListApi.Services;
 
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy  =>
+        {
+            policy.WithOrigins("http://localhost:3000")
+                                    .AllowAnyHeader()
+                                    .AllowAnyMethod()
+                                    .AllowCredentials();
+        });
+});
 // Add services to the container.
 var settings = builder.Configuration.GetSection("TodoListDataBase");
 builder.Services.Configure<TodoListDataBaseSettings>(settings);
@@ -24,6 +36,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
